688dda1ec25eb6c885e528cf342574bf50e168cc
mo <1827568954@qq.com>
2024-08-11 20:53:04 +0800

    修复单发测试页面无法接收到群组消息的bug；优化UI，增加单发测试页面的消息通知栏滚动条自动到底。

--- src/main/java/cn/redcoral/chattest/MessageUtil.java
+++ src/main/java/cn/redcoral/chattest/MessageUtil.java
@@ -33,11 +33,6 @@ public class MessageUtil implements MessagePlusBase {
     }
 
     @Override
-    public void onFailedMessage(Message message) {
-
-    }
-
-    @Override
     public void onMessageBySystem(String senderId, String message) {
     }
 }
